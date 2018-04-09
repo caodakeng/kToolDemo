74c15327fc2cc80716eae27adc3788c2da9e19ef
荒天帝 <1027887839@qq.com>
2018-04-09 10:08:19 +0800

    Podfile

---
+++ kToolDemo/kToolDemo/kClass/kItem/kMain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kToolDemo
+//
+//  Created by 曹翔 on 2018/4/8.
+//  Copyright © 2018年 CX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kToolDemo/kToolDemoTests/kToolDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  kToolDemoTests.m
+//  kToolDemoTests
+//
+//  Created by 曹翔 on 2018/4/8.
+//  Copyright © 2018年 CX. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface kToolDemoTests : XCTestCase
+
+@end
+
+@implementation kToolDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
